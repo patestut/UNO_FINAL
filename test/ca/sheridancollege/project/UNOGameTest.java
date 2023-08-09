de5ef34f993f76fb78e762e341b7f2772b7379bb
gugi <133898074+gugi-g@users.noreply.github.com>
2023-08-09 18:16:33 -0400

    Update UNOGameTest.java

--- test/ca/sheridancollege/project/UNOGameTest.java
+++ test/ca/sheridancollege/project/UNOGameTest.java
@@ -34,46 +34,37 @@ public class UNOGameTest {
         //unoGame.setPlayers(players);
     }
 
-    // Good Test: Ensure the game initializes properly
     @Test
     public void testGoodInitialization() {
         assertNotNull(unoGame);
     }
 
-    // Good Test: Ensure the currentColor is set correctly after initialization
-    @Test
-   // public void testGoodCurrentColorAfterInitialization() {
-       // assertEquals("", unoGame.getCurrentColor());
-   // }
 
-    // Boundary Test: Ensure the playerTurn method can be executed without exceptions
-   // @Test
+    @Test
     public void testBoundaryPlayerTurn() {
         // Additional assertions can be made based on specific player turn effects
         unoGame.playerTurn(player1);
     }
 
-    // Boundary Test: Ensure the handleSkippedTurn method can be executed without exceptions
+
     @Test
     public void testBoundaryHandleSkippedTurn() {
         unoGame.handleSkippedTurn(player1);
-        // Additional assertions can be made based on specific skipped turn effects
     }
 
-    // Boundary Test: Ensure the checkGameEnd method returns false when player has cards
+    
     @Test
     public void testBoundaryCheckGameEndWithCards() {
         player1.drawCard(new UNOCard("Red", "5"));
         assertFalse(unoGame.checkGameEnd(player1));
     }
-
-    // Boundary Test: Ensure the checkGameEnd method returns true when player has no cards
+    
     @Test
     public void testBoundaryCheckGameEndWithoutCards() {
         assertTrue(unoGame.checkGameEnd(player1));
     }
 
-    // Good Test: Ensure the calculateScore method returns the correct score
+    
     @Test
     public void testGoodCalculateScore() {
         player1.drawCard(new UNOCard("Red", "5"));
@@ -81,12 +72,12 @@ public class UNOGameTest {
         assertEquals(15, unoGame.calculateScore(player1));
     }
 
-    // Good Test: Ensure the declareWinner method can be executed without exceptions
+
     @Test
     public void testGoodDeclareWinner() {
         player1.drawCard(new UNOCard("Red", "5"));
         player2.drawCard(new UNOCard("Blue", "Skip"));
         unoGame.declareWinner();
-        // Additional assertions can be made based on specific winner calculation
+        
     }
 }
